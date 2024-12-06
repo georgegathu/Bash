62c6f125a249f91dbcde1ed32f87e56175cbfb5e
georgegathu <ggeorge.gathu@gmail.com>
2024-12-06 21:36:51 +0300

    2nd trial

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-first_name
-second_name
+# Prompt the user for the first and last name
+read -p "Enter your first name: " first_name
+read -p "Enter your second name: " second_name
+read -p "Enter your last name: " last_name
 
-your_name = first_name + second_name
+# Concatenate the names
+your_name="$first_name $second_name $last_name"
 
-echo 
+# Print the full name
+echo "Your full name is: $your_name"
