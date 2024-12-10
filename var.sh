814b45a9c3f5dc99144960fa112d5ff90a0c7bc4
georgegathu <ggeorge.gathu@gmail.com>
2024-12-10 15:31:36 +0300

    Variables

--- var.sh
+++ var.sh
@@ -9,4 +9,11 @@ echo $my_var
 	#or use {} to emphasise the variable
 echo ${my_var}
 
+#Bash Data Types
+#1.Strings (These are a sequence of characters)
+name="GG"
+echo "Hello, $name"
 
+#2.Integers(can be used to perform arithmetic operations on integers)
+age=22
+echo "In 10 years, you will be $((age+10)) years old"
