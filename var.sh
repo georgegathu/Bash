7086439958a98429c29147f76dca9bbfb2dbcc28
georgegathu <ggeorge.gathu@gmail.com>
2024-12-10 16:06:47 +0300

    Variables

--- var.sh
+++ var.sh
@@ -11,9 +11,17 @@ echo ${my_var}
 
 #Bash Data Types
 #1.Strings (These are a sequence of characters)
-name="GG"
-echo "Hello, $name"
+STRING="TryHackMe"
+echo "Hackers love to learn from, $STRING"
 
 #2.Integers(can be used to perform arithmetic operations on integers)
 age=22
 echo "In 10 years, you will be $((age+10)) years old"
+
+#3. We can also use variables to ask and store users input
+echo "What's your name?"
+read name
+echo "What is your age?"
+read age
+
+echo "Wow $name, you are $age years old!"
