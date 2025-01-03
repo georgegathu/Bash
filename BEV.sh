fc60143c004a210747ec069f2a2dc3a88022761d
georgegathu <ggeorge.gathu@gmail.com>
2025-01-03 22:36:08 +0300

    some of the default environment variables available in Bash

---
+++ BEV.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Bash Environment Variables
+
+<<'###BLOCK-COMMENT'
+
+BASH_VERSION - Returns the bash version running
+BASHPID - Returns the process ID of the current bash process
+GROUPS - Returns a list of groups the running user is a member of
+HOSTNAME - Returns the name of the Host
+OSTYPE - Returns the type of Operating System
+PWD - Returns the current working directory
+RANDOM - Returns a random number between 0 - 32,767
+UID - Returns the User ID of the current user
+SHELL - Returns the full pathname to the shell
+
+###BLOCK-COMMENT
---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Backup script to copy files from source to destination
+src="/path/to/source"
+dest="/path/to/destination"
+cp -r $src $dest
+echo "Backup completed!"
