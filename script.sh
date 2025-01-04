1ec28734802bb5410ea7eaff8cfbffa4955462b2
georgegathu <ggeorge.gathu@gmail.com>
2025-01-04 23:08:06 +0300

    This script  creates a directory and creates a file

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+# All this script does is create a directory, create a file 
+# within the directory, and then list the content of the directory. 
+mkdir directory 
+touch mydirectory/myfile 
+ls -l mydirectory 
